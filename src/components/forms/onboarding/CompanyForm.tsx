2859c255726b01bcc47f70055a8c127251401be7
Julian Odilibe <graftwerx@gmail.com>
2025-07-16 06:59:29 +0100

    onboarding pt1

---
+++ src/components/forms/onboarding/CompanyForm.tsx
@@ -0,0 +1,95 @@
+import { countryList } from "@/app/utils/countriesList";
+import { companySchema } from "@/app/utils/zodSchema";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+export function CompanyForm() {
+  const form = useForm<z.infer<typeof companySchema>>({
+    resolver: zodResolver(companySchema),
+    defaultValues: {
+      about: "",
+      location: "",
+      logo: "",
+      name: "",
+      website: "",
+      xAccount: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Company Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Location</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select Location" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Worldwide</SelectLabel>
+                      <SelectItem value="world">
+                        <span>Worldwide / Remote</span>
+                      </SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Location</SelectLabel>
+                      {countryList.map((country) => (
+                        <SelectItem key={country.code} value={country.name}>
+                          <span>{country.flagEmoji}</span>
+                          <span className="pl-2">{country.name}</span>
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
+}
---
+++ src/components/forms/onboarding/OnboardingForm.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { useState } from "react";
+import { UserTypeSelection } from "./UserTypeForm";
+import { CompanyForm } from "./CompanyForm";
+
+type UserSelectionType = "company" | "jobseeker" | null;
+
+export function OnboardingForm() {
+  const [step, setStep] = useState(1);
+  const [userType, setUserType] = useState<UserSelectionType>(null);
+
+  function handleUserTypeSelection(type: UserSelectionType) {
+    setUserType(type);
+    setStep(2);
+  }
+  function renderStep() {
+    switch (step) {
+      case 1:
+        return <UserTypeSelection onSelect={handleUserTypeSelection} />;
+
+      case 2:
+        return userType === "company" ? (
+          <CompanyForm />
+        ) : (
+          <p>User is a jobseeker..</p>
+        );
+
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <>
+      <div className="flex items-center gap-4 mb-10">
+        <Image src="/emblem.png" alt="Logo" width={40} height={40} />
+        <h1 className="text-4xl font-bold">
+          <span className="text-primary">Job</span>Werx
+        </h1>
+      </div>
+      <Card className="max-w-lg w-full">
+        <CardContent className="p-6">{renderStep()}</CardContent>
+      </Card>
+    </>
+  );
+}
