da6d991eccea24a8bb2c8d4d1801d4c15105c73a
Julian Odilibe <graftwerx@gmail.com>
2025-07-17 12:26:13 +0100

    arcjet

--- src/app/utils/actions.ts
+++ src/app/utils/actions.ts
@@ -5,9 +5,30 @@ import { requireUser } from "./requireUser";
 import { companySchema, jobSeekerSchema } from "./zodSchema";
 import {prisma} from "./db"
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet.withRule(
+    shield({
+        mode: "LIVE",
+    })
+    
+).withRule(
+    detectBot({
+        mode: "LIVE",
+        allow: []
+    })
+)
 
 export async function createCompany(data: z.infer<typeof companySchema>){
     const session = await requireUser();
+
+    const req = await request()
+    const decision = await aj.protect(req)
+
+    if(decision.isDenied()){
+        throw new Error("Forbidden")
+    }
 
     const validateData = companySchema.parse(data);
 
@@ -30,6 +51,13 @@ export async function createCompany(data: z.infer<typeof companySchema>){
 
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>){
     const user = await requireUser();
+
+        const req = await request()
+    const decision = await aj.protect(req)
+
+    if(decision.isDenied()){
+        throw new Error("Forbidden")
+    }
 
     const validateData = jobSeekerSchema.parse(data);
 
